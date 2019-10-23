1c7cc8aee31a620f6286a61d64d57535b723b64e
Ironolife <40685147+Ironolife@users.noreply.github.com>
2019-10-23 20:55:33 +0800

    Added method to compare Histograms by regions

--- ImageRetrieval/Main.cpp
+++ ImageRetrieval/Main.cpp
@@ -15,10 +15,57 @@
 using namespace std;
 using namespace cv;
 
-#define IMAGE_folder "H:\\dataset" // change to your folder location
+#define IMAGE_folder "D:\\dataset" // change to your folder location
 #define IMAGE_LIST_FILE "dataset1" //the dataset1 for retrieval
 #define output_LIST_FILE "searchResults" //the search results will store in this file
 #define SEARCH_IMAGE "999.jpg" //change from 990 to 999 as the search images to get your output
+
+struct features {
+	Mat histograms[5];
+};
+
+features getHist(Mat img)
+{
+	//Convert to HSV
+	Mat hsv;
+	cvtColor(img, hsv, COLOR_BGR2HSV);
+
+	//Find image dimensions & centers
+	int width = hsv.size().width, height = hsv.size().height;
+	int centerX = width / 2, centerY = height / 2;
+
+	//Split image into 5 segments
+	Mat segMasks[5];
+	int segRectPoints[4][4] = {
+		{ 0, centerX, 0, centerY },
+		{centerX, width, 0, centerY},
+		{ 0, centerX, centerY, height},
+		{centerX, width, centerY, height},
+	};
+
+	Mat centerMask = Mat::zeros(hsv.size(), 0);
+	ellipse(centerMask, Point(centerX, centerY), Point(centerX * 0.75, centerY * 0.75), 0, 0, 360, Scalar(255, 255, 255), -1);
+
+	for (int i = 0; i < 4; i++) {
+		segMasks[i] = Mat::zeros(hsv.size(), 0);
+		rectangle(segMasks[i], Point(segRectPoints[i][0], segRectPoints[i][2]), Point(segRectPoints[i][1], segRectPoints[i][3]), Scalar(255, 255, 255), -1);
+		subtract(segMasks[i], centerMask, segMasks[i]);
+	}
+
+	//Calculate histogram for each region
+	int hBins = 8, sBins = 12;
+	int histSize[] = { hBins, sBins };
+	float hRanges[] = { 0, 180 }, sRanges[] = { 0, 256 };
+	const float* ranges[] = { hRanges, sRanges };
+	int channels[] = { 0, 1 };
+	features features;
+	for (int i = 0; i < 5; i++) {
+		calcHist(&hsv, 1, channels, segMasks[i], features.histograms[i], 2, histSize, ranges);
+		normalize(features.histograms[i], features.histograms[i], 0, 1, NORM_MINMAX, -1, Mat());
+	}
+
+	return features;
+}
 
 double featureMatching(Mat img1, Mat img2)
 {
@@ -67,53 +114,35 @@ double featureMatching(Mat img1, Mat img2)
 	return score;
 }
 
-int findCircle(Mat img1, Mat img2)
+int findCircle(Mat img)
 {
 	// convert to grayscale
-	Mat gray1, gray2;
-	cvtColor(img1, gray1, COLOR_BGR2GRAY);
-	cvtColor(img2, gray2, COLOR_BGR2GRAY);
-
-	//// resize image
-	//int w = gray1.cols, h = gray1.rows;
-	//resize(gray2, gray2, gray1.size());
+	Mat gray;
+	cvtColor(img, gray, COLOR_BGR2GRAY);
 
 	// apply blur
-	medianBlur(gray1, gray1, 5);
-	medianBlur(gray2, gray2, 5);
+	medianBlur(gray, gray, 5);
 
 	// find circles
-	vector<Vec3f> circles1, circles2;
-	HoughCircles(gray1, circles1, CV_HOUGH_GRADIENT, 1, gray1.rows / 16, 30, 100, 150, 400);
-	HoughCircles(gray2, circles2, CV_HOUGH_GRADIENT, 1, gray2.rows, 60, 75, 50, 0);
+	vector<Vec3f> circles;
+	HoughCircles(gray, circles, CV_HOUGH_GRADIENT, 1, gray.rows/8, 60, 75, 50, 0);
 
-	for (size_t i = 0; i < circles1.size(); i++)
+	for (size_t i = 0; i < circles.size(); i++)
 	{
-		Point center(cvRound(circles1[i][0]), cvRound(circles1[i][1]));
-		int radius = cvRound(circles1[i][2]);
+		Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
+		int radius = cvRound(circles[i][2]);
 		// circle center
-		circle(gray1, center, 3, Scalar(0, 255, 0), -1, 8, 0);
+		circle(gray, center, 3, Scalar(0, 255, 0), -1, 8, 0);
 		// circle outline
-		circle(gray1, center, radius, Scalar(0, 0, 255), 3, 8, 0);
-	}
-	for (size_t i = 0; i < circles2.size(); i++)
-	{
-		Point center(cvRound(circles2[i][0]), cvRound(circles2[i][1]));
-		int radius = cvRound(circles2[i][2]);
-		// circle center
-		circle(gray2, center, 3, Scalar(0, 255, 0), -1, 8, 0);
-		// circle outline
-		circle(gray2, center, radius, Scalar(0, 0, 255), 3, 8, 0);
+		circle(gray, center, radius, Scalar(0, 0, 255), 3, 8, 0);
 	}
 
-	namedWindow("input", CV_WINDOW_AUTOSIZE);
-	imshow("input", gray1);
-	namedWindow("db", CV_WINDOW_AUTOSIZE);
-	imshow("db", gray2);
+	namedWindow("circle", CV_WINDOW_AUTOSIZE);
+	imshow("circle", gray);
 
 	waitKey(0);
 
-	return 0;
+	return circles.size();
 }
 
 //Compute similarity
@@ -121,9 +150,14 @@ double compareImgs(Mat img1, Mat img2)
 {
 	double score = 0;
 
-	findCircle(img1, img2);
+	features features1 = getHist(img1);
+	features features2 = getHist(img2);
 
-	//score = featureMatching(img1, img2);
+	findCircle(img2);
+
+	for (int i = 0; i < 5; i++) {
+		score += compareHist(features1.histograms[i], features2.histograms[i], 1);
+	}
 
 	return score;
 }
@@ -139,7 +173,7 @@ int main(int argc, char** argv)
 	const int db_size = 1000;
 	int db_id = 990;
 
-	const int score_size = 10; //change this to control return top n images
+	const int score_size = 10; //Change this to control return top n images
 	double minscore[score_size] = { DBL_MAX };
 	int minFilename[score_size];
 
@@ -147,7 +181,7 @@ int main(int argc, char** argv)
 	Mat min_img;
 
 	sprintf_s(tempname, filename_len, "%s\\%s\\%s", IMAGE_folder, IMAGE_LIST_FILE, SEARCH_IMAGE);
-	src_input = imread(tempname); // read input image
+	src_input = imread(tempname); //Read input image
 	if (!src_input.data)
 	{
 		printf("Cannot find the input image!\n");
@@ -159,7 +193,7 @@ int main(int argc, char** argv)
 	//Read Database
 	for (db_id; db_id<db_size; db_id++) {
 		sprintf_s(tempname, filename_len, "%s\\%s\\%d.jpg", IMAGE_folder, IMAGE_LIST_FILE, db_id);
-		db_img = imread(tempname); // read database image
+		db_img = imread(tempname); //Read database image
 		if (!db_img.data)
 		{
 			printf("Cannot find the database image number %d!\n", db_id + 1);
@@ -167,10 +201,10 @@ int main(int argc, char** argv)
 			return -1;
 		}
 
-		// Apply the pixel-by-pixel comparison method
+		//Apply the pixel-by-pixel comparison method
 		double tempScore = compareImgs(src_input, db_img);
 
-		//store the top k min score ascending
+		//Store the top k min score ascending
 		for (int k = 0; k<score_size; k++) {
 			if (tempScore < minscore[k]) {
 				for (int k1 = score_size - 1; k1>k; k1--) {
@@ -184,7 +218,7 @@ int main(int argc, char** argv)
 		}
 	}
 
-	//read the top k max score image and write them to the a designated folder
+	//Read the top k max score image and write them to the a designated folder
 	for (int k = 0; k<score_size; k++) {
 		sprintf_s(minimg_name, filename_len, "%s\\%s\\%d.jpg", IMAGE_folder, IMAGE_LIST_FILE, minFilename[k]);
 		min_img = imread(minimg_name);
@@ -194,7 +228,7 @@ int main(int argc, char** argv)
 		//imshow(tempname,max_img);
 	}
 
-	//output your precesion and recall (the ground truth are from 990 to 999)
+	//Output your precesion and recall (the ground truth are from 990 to 999)
 	int count = 0;
 	for (int k = 0; k<score_size; k++) {
 		if (minFilename[k] >= 990 && minFilename[k] <= 999) {
@@ -208,7 +242,7 @@ int main(int argc, char** argv)
 
 	printf("Done \n");
 
-	// Wait for the user to press a key in the GUI window.
+	//Wait for the user to press a key in the GUI window.
 	//Press ESC to quit
 	int keyValue = 0;
 	while (keyValue >= 0)
